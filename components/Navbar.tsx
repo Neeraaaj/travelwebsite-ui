31161ba9787bdb7cb5626eff549c010791ecbfd3
Neeraj Patil <110386298+Neeraaaj@users.noreply.github.com>
2025-03-30 19:35:12 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,15 +22,6 @@ const Navbar = () => {
                 })}
             </ul>
 
-            <div className='lg:flexCenter hidden'>
-                <Button 
-                    type="button" 
-                    title="Login" 
-                    icon="/user.svg"
-                    variant="btn_dark_green"
-                />
-            </div>
-
             <Image 
                 src={"/menu.svg"} 
                 alt="menu" 
